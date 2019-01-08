889ce49c16e31639c842c2d40f27128047614128
cloud16 <caringal1820662@mkt.ceu.edu.ph>
2019-01-08 13:29:56 +0800

    JFrame

--- BasicElements.java
+++ BasicElements.java
@@ -4,6 +4,9 @@ public class BasicElements {
 
 	public static void main(String args[]){
 		 JFrame myFrame = new JFrame();
+		 JButton myButton = new Jbutton();
+        myFrame.setLayout(null);
+		 myFrame.add(myButton);
 
          myFrame.setDeafultCLoseOperation(myFrame.EXIT_ON_CLose);
 		 myFrame.setVisible(true);
