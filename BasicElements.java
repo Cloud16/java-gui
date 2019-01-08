85593ddc3bf5dd8f9a81df783a131056a4bc01b9
cloud16 <caringal1820662@mkt.ceu.edu.ph>
2019-01-08 12:53:42 +0800

    JFRame

--- BasicElements.java
+++ BasicElements.java
@@ -1,6 +1,13 @@
+import javax.swing.JFrame
 public class BasicElements {
+	 
 
 	public static void main(String args[]){
+		 JFrame myFrame = new JFrame();
+
+         myFrame.setDeafultCLoseOperation(myFrame.EXIT_ON_CLose);
+		 myFrame.setVisible(true);
+		 myFrame.setSize(900, 600);
 		System.out.println("basic java elements");
 	}
 }
